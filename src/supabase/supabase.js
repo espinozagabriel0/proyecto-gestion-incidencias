936ac872daf9043326f8b58104f7da474d86513c
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2025-04-01 20:08:08 +0200

    variables de entorno supabase

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rckoapsickeinxcrsphi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJja29hcHNpY2tlaW54Y3JzcGhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MjYyNzAsImV4cCI6MjA1ODUwMjI3MH0.Dl4J2zAkE6pBrqpjYd7T0xpVkgpw8-XU_Am7O1hL56M';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
